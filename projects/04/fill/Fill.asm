f50cfec865b7948d59057701865bb56d1a10aaac
Alexander A Bouman <alex.bouman@gmail.com>
2023-07-08 19:33:56 -0500

    Kinda working fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,9 +6,70 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+    @8192
+    D=A
+    @screen_words
+    M=D
+
+    @last_keyboard_state
+    M=0
+    @curr_keyboard_state
+    M=0
+
+    @black_screen_word
+    M=-1
+
+
+(KEYBOARD_LOOP)
+    @screen_itr
+    M=0
+
+    // Load keyboard state.
+    @KBD
+    D=M
+    @keyboard_state
+    M=D
+    D=M
+    // If keyboard has any button pressed, loop.
+    @KEYBOARD_LOOP
+    D;JEQ
+
+(SCREEN_LOOP)
+    // Check if iterator is larger than R1.
+    @screen_words
+    D=M
+    @screen_itr
+    D=D-M
+    @SCREEN_END
+    D;JLE
+
+    @screen_itr
+    D=M
+    @SCREEN
+    D=D+A
+    @screen_index
+    M=D
+
+    @black_screen_word
+    D=M
+    @screen_index
+    A=M
+    M=D
+
+    // Increment iterator and loop.
+    @screen_itr
+    M=M+1
+    @SCREEN_LOOP
+    0;JMP
+
+(SCREEN_END)
+
+    @KEYBOARD_LOOP
+    0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,40 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+    // Iterator starts at 0.
+    @itr
+    M=0
+    // Sum start at 0.
+    @sum
+    M=0
+
+(LOOP)
+    // Check if iterator is larger than R1.
+    @R1
+    D=M
+    @itr
+    D=D-M
+    @END
+    D;JLE
+
+    // Increase sum.
+    @R0
+    D=M
+    @sum
+    M=D+M
+
+    // Increment iterator and loop.
+    @itr
+    M=M+1
+    @LOOP
+    0;JMP
+
+(END)
+    // Copy product into R3.
+    @sum
+    D=M
+    @R2
+    M=D
+
+    @END
+    0;JMP
