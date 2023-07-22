972300d53709960ce6e04742dbe9907a10536013
Alexander A Bouman <alex.bouman@gmail.com>
2023-07-22 12:57:53 -0500

    Working arthimetic mappings

--- projects/07/vm_translator.jl
+++ projects/07/vm_translator.jl
@@ -18,25 +18,23 @@ Base.length(b::StaticDictionary{N}) where {N} = N
 # %%
 # pop local i
 # @i
-# D = A
+# D = A     // D = i
 # @LCL
-# D = D + A
+# D = D + M // D = i + RAM[LCL]
 # @R14
-# M = D     # Store 5+i in R14
+# M = D     // RAM[14] = i + RAM[LCL]
 # @SP
+# AM=M-1
+# D = M     // D = RAM[SP-1]
+# @R14
 # A = M
-# D = M
-# @R14
-# A = M     # Access RAM[5+i]
-# M = D     # Store
-# @SP
-# M = M - 1
+# M = D     // RAM[i + RAM[LCL]] = RAM[SP-1]
 
 # pop argument i
 # @i
 # D = A
 # @ARG
-# D = D + A
+# D = D + M
 # @R14
 # M = D     # Store 5+i in R14
 # @SP
@@ -98,7 +96,8 @@ Base.length(b::StaticDictionary{N}) where {N} = N
 
 # pop pointer 0
 # @SP
-# D = M
+# AM = M - 1
+# D = M         // D = RAM[SP-1]
 # @THIS
 # M = D
 # @SP
@@ -131,15 +130,12 @@ Base.length(b::StaticDictionary{N}) where {N} = N
 
 # push local i
 # @i
-# D = A
+# D = A      // D = i
 # @LCL
-# A = D + A
-# D = M
+# D = D + M  // D = RAM[i + RAM[LCL]]
 # @SP
-# A = M
-# M = D
-# @SP
-# M = M + 1
+# AM=M+1
+# M = D      // RAM[SP] = RAM[i + RAM[LCL]]
 
 # push argument i
 # @i
@@ -218,215 +214,188 @@ Base.length(b::StaticDictionary{N}) where {N} = N
 
 # add
 # @SP
-# A = M
+# AM=M-1
 # D = M
 # @SP
-# M = M - 1
-# A = M
+# AM = M - 1
 # M = D + M
+# @SP
+# M = M + 1
 
 # sub
 # @SP
-# A = M
+# AM = M - 1
 # D = M
 # @SP
-# M = M - 1
-# A = M
+# AM = M - 1
 # M = D - M
 # M = -M
+# @SP
+# M = M + 1
 
 # neg
 # @SP
-# A = M
+# AM = M - 1
 # M = -M
+# @SP
+# M = M + 1
 
 # eq
 # @SP
-# A = M
-# D = M      // D = RAM[SP]
-# @R13
-# M = D      // RAM[13] = RAM[SP]
+# AM = M - 1
+# D = M       // D = RAM[SP-1]
 # @SP
-# M = M - 1  // SP = SP - 1
-# A = M
-# D = M      // D = RAM[SP-1]
-# @R14
-# M = D      // RAM[14] = RAM[SP-1]
-# D = M      // D = RAM[SP-1]
-# @R15
-# M = D      // RAM[15] = RAM[SP-1]
-# @R13
-# D = M      // D = RAM[13] = RAM[SP]
-# @R15
-# D = D & M  // D = RAM[SP] && RAM[15] = RAM[SP] && RAM[SP-1]
+# AM = M - 1
+# D = D - M   // D = RAM[SP-1] - RAM[SP-2]
+# D = -D      // D = RAM[SP-2] - RAM[SP-1]
 # @SP
 # A = M
-# M = D      // RAM[SP] = RAM[SP] && RAM[SP-1]
-# @R14
-# D = M      // D = RAM[14] = RAM[SP-1]
-# @R15
-# M = D      // RAM[15] = RAM[SP-1]
-# @R13
-# D = M      // D = RAM[13] = RAM[SP]
-# @R15
-# M = D & M  // RAM[15] = RAM[SP] || RAM[15] = RAM[SP] || RAM[SP-1]
-# D = !M     // D = !RAM[15] = !(RAM[SP] || RAM[SP-1]) = !RAM[SP] && !RAM[SP-1]
+# M = 0
+# @NOT_EQUAL_i
+# D;JNE       // Jump to (NOT_EQUAL_i) if RAM[SP-2] - RAM[SP-1] != 0
 # @SP
 # A = M
-# M = D||M   // RAM[SP] = (!RAM[SP] && !RAM[SP-1]) || RAM[SP] = (!RAM[SP] && !RAM[SP-1]) || (RAM[SP] && RAM[SP-1])
+# M = -1
+# (NOT_EQUAL_i)
+# @SP
+# M=M+1
 
 # gt
 # @SP
-# A = M
-# D = M      // D = RAM[SP]
-# @R13
-# M = D      // RAM[13] = RAM[SP]
+# AM = M - 1
+# D = M       // D = RAM[SP-1]
 # @SP
-# M = M - 1  // SP = SP - 1
-# A = M
-# D = M      // D = RAM[SP-1]
-# @R14
-# M = D      // RAM[14] = RAM[SP-1]
-# D = M      // D = RAM[SP-1]
-# @R13
-# D = M      // D = RAM[13] = RAM[SP]
+# AM = M - 1
+# D = D - M   // D = RAM[SP-1] - RAM[SP-2]
+# D = -D      // D = RAM[SP-2] - RAM[SP-1]
 # @SP
 # A = M
-# M = D - M  // RAM[SP] = D - RAM[SP-1] = RAM[SP] - RAM[SP-1]
+# M = 0
+# @LESS_THAN_OR_EQUAL_i
+# D;JLE       // Jump to (LESS_THAN_OR_EQUAL_i) if RAM[SP-2] - RAM[SP-1] != 0
+# @SP
+# A = M
+# M = -1
+# (LESS_THAN_OR_EQUAL_i)
+# @SP
+# M=M+1
 
+# and
+# @SP
+# AM = M - 1
+# D = M       // D = RAM[SP-1]
+# @SP
+# AM = M - 1
+# M = D & M   // RAM[SP-2] = RAM[SP-1] & RAM[SP-2]
+# @SP
+# M=M+1
+
+# not
+# @SP
+# AM = M - 1
+# M = !M       // RAM[SP-1] = !RAM[SP-1]
+# @SP
+# M=M+1
 
 
 # %%
-stack_operation_map = Dict{Regex,Function}(
-    r"pop local ([-+]?\d+)"    => (_i -> "@$(_i)\nD=A\n@LCL\nD=D+A\n@R14\nM=D\n@SP\nA=M\nD=M\n@R14\nA=M\nM=D\n@SP\nM=M-1\n"),
-    r"pop argument ([-+]?\d+)" => (_i -> "@$(_i)\nD=A\n@ARG\nD=D+A\n@R14\nM=D\n@SP\nA=M\nD=M\n@R14\nA=M\nM=D\n@SP\nM=M-1\n"),
-    r"pop this ([-+]?\d+)"     => (_i -> "@$(_i)\nD=A\n@THIS\nD=D+A\n@R14\nM=D\n@SP\nA=M\nD=M\n@R14\nA=M\nM=D\n@SP\nM=M-1\n"),
-    r"pop that ([-+]?\d+)"     => (_i -> "@$(_i)\nD=A\n@THAT\nD=D+A\n@R14\nM=D\n@SP\nA=M\nD=M\n@R14\nA=M\nM=D\n@SP\nM=M-1\n"),
-    r"pop temp ([0-7])"        => (_i -> "@$(_i)\nD=A\n@R5\nD=D+A\n@R14\nM=D\n@SP\nA=M\nD=M\n@R14\nA=M\nM=D\n@SP\nM=M-1\n"),
-    r"pop pointer 0"           => (_i -> "@SP\nD=M\n@THIS\nM=D\n@SP\nM=M-1\n"),
-    r"pop pointer 1"           => (_i -> "@SP\nD=M\n@THAT\nM=D\n@SP\nM=M-1\n"),
+push_pop_map = Dict{Regex,Function}(
+    r"pop local ([-+]?\d+)"    => (_i -> "@$(_i)\nD=A\n@LCL\nD=D+M\n@R14\nM=D\n@SP\nAM=M-1\nD=M\n@R14\nA=M\nM=D\n"),
+    r"pop argument ([-+]?\d+)" => (_i -> "@$(_i)\nD=A\n@ARG\nD=D+M\n@R14\nM=D\n@SP\nAM=M-1\nD=M\n@R14\nA=M\nM=D\n"),
+    r"pop this ([-+]?\d+)"     => (_i -> "@$(_i)\nD=A\n@THIS\nD=D+M\n@R14\nM=D\n@SP\nAM=M-1\nD=M\n@R14\nA=M\nM=D\n"),
+    r"pop that ([-+]?\d+)"     => (_i -> "@$(_i)\nD=A\n@THAT\nD=D+M\n@R14\nM=D\n@SP\nAM=M-1\nD=M\n@R14\nA=M\nM=D\n"),
+    r"pop temp ([0-7])"        => (_i -> "@$(_i)\nD=A\n@R5\nD=D+M\n@R14\nM=D\n@SP\nAM=M-1\nD=M\n@R14\nA=M\nM=D\n"),
+    r"pop pointer 0"           => (_i -> "@SP\nAM=M-1\nD=M\n@THIS\nM=D\n@SP\nM=M-1\n"),
+    r"pop pointer 1"           => (_i -> "@SP\nAM=M-1\nD=M\n@THAT\nM=D\n@SP\nM=M-1\n"),
     r"pop static \b(0|[1-9]\d?|1\d\d|2[0-3]\d)\b" => (_i -> "@SP\nD=M\n@Foo.$(_i)\nM=D\n@SP\nM=M-1\n"),
 
     r"push constant ([-+]?\d+)" => (_i -> "@$(_i)\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"),
-    r"push local ([-+]?\d+)"    => (_i -> "@$(_i)\nD=A\n@LCL\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"),
-    r"push argument ([-+]?\d+)" => (_i -> "@$(_i)\nD=A\n@ARG\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"),
-    r"push this ([-+]?\d+)"     => (_i -> "@$(_i)\nD=A\n@THIS\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"),
-    r"push that ([-+]?\d+)"     => (_i -> "@$(_i)\nD=A\n@THAT\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"),
-    r"push temp ([0-7])"        => (_i -> "@$(_i)\nD=A\n@R5\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"),
+    r"push local ([-+]?\d+)"    => (_i -> "@$(_i)\nD=A\n@LCL\nD=D+M\n@SP\nAM=M+1\nM=D\n"),
+    r"push argument ([-+]?\d+)" => (_i -> "@$(_i)\nD=A\n@ARG\nD=D+M\n@SP\nAM=M+1\nM=D\n"),
+    r"push this ([-+]?\d+)"     => (_i -> "@$(_i)\nD=A\n@THIS\nD=D+M\n@SP\nAM=M+1\nM=D\n"),
+    r"push that ([-+]?\d+)"     => (_i -> "@$(_i)\nD=A\n@THAT\nD=D+M\n@SP\nAM=M+1\nM=D\n"),
+    r"push temp ([0-7])"        => (_i -> "@$(_i)\nD=A\n@R5\nD=D+M\n@SP\nAM=M+1\nM=D\n"),
     r"push pointer 0"           => (_i -> "@THIS\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"),
     r"push pointer 1"           => (_i -> "@THAT\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"),
     r"push static \b(0|[1-9]\d?|1\d\d|2[0-3]\d)\b" => (_i -> "@Foo.$(_i)\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"),
 )
 
-arithmetic_operation_map = Dict{Regex,Function}()
+arithmetic_logical_map = Dict{Regex,Function}(
+    r"add" => (_i -> "@SP\nAM=M-1\nD=M\n@SP\nAM=M-1\nM=D+M\n@SP\nM=M+1\n"),
+    r"sub" => (_i -> "@SP\nAM=M-1\nD=M\n@SP\nAM=M-1\nM=D-M\nM=-M\n@SP\nM=M+1\n"),
+    r"neg" => (_i -> "@SP\nAM=M-1\nM=-M\n@SP\nM=M+1\n"),
 
-# %%
-in_buf = IOBuffer("push constant 10
-pop local 0
-push constant 21
-push constant 22
-pop argument 2
-pop argument 1
-push constant 36
-")
-out_buf = IOBuffer()
+    r"eq"  => (_i -> "@SP\nAM=M-1\nD=M\n@SP\nAM=M-1\nD=D-M\n@SP\nA=M\nM=0\n@NOT_EQUAL_$(_i)\nD;JNE\n@SP\nA=M\nM=-1\n(NOT_EQUAL_$(_i))\n@SP\nM=M+1\n"),
+    r"gt"  => (_i -> "@SP\nAM=M-1\nD=M\n@SP\nAM=M-1\nD=D-M\nD=-D\n@SP\nA=M\nM=0\n@LESS_THAN_OR_EQUAL_$(_i)\nD;JLE\n@SP\nA=M\nM=-1\n(LESS_THAN_OR_EQUAL_$(_i))\n@SP\nM=M+1\n"),
+    r"lt"  => (_i -> "@SP\nAM=M-1\nD=M\n@SP\nAM=M-1\nD=D-M\nD=-D\n@SP\nA=M\nM=0\n@GREATER_THAN_OR_EQUAL_$(_i)\nD;JGE\n@SP\nA=M\nM=-1\n(GREATER_THAN_OR_EQUAL_$(_i))\n@SP\nM=M+1\n"),
 
+    r"and" => (_i -> "@SP\nAM=M-1\nD=M\n@SP\nAM=M-1\nM=D&M\n@SP\nM=M+1\n"),
+    r"or"  => (_i -> "@SP\nAM=M-1\nD=M\n@SP\nAM=M-1\nM=D|M\n@SP\nM=M+1\n"),
+    r"not" => (_i -> "@SP\nAM=M-1\nM=!M\n@SP\nM=M+1\n"),
+)
 
+function remove_comments(line::AbstractString, )
+    index = findfirst("//", line)
+    if index !== nothing
+        return strip(line[1:(index[1]-1)])
+    end
+    return line
+end
 
-for line in eachline(in_buf)
-    for (key, value) in stack_operation_map
+function translate_push_pop(line::AbstractString, stream_out::IO, )
+    for (key, value) in push_pop_map
         if occursin(key, line)
-            write(out_buf, value(match(key, line).captures[1]))
-            continue
+            reg_match = match(key, line)
+            (reg_match === nothing) && error("Failed to parse line : \"$line\"")
+
+            write(stream_out, value(isempty(reg_match.captures) ? nothing : reg_match.captures[1]))
+            break
         end
     end
 end
 
-print(String(out_buf.data))
-
-# %%
-function translate_pop(stream_in::IO, stream_out::IO,)
-    for (key, value) in stack_operation_map
+function translate_arithmetic_logic(line::AbstractString, stream_out::IO, arithmetic_op_counts::Dict{Regex,Int})
+    for (key, value) in arithmetic_logical_map
         if occursin(key, line)
-            write(stream_out, value(match(key, line).captures[1]))
+            write(stream_out, value(arithmetic_op_counts[key]))
+            arithmetic_op_counts[key] += 1
+            break
         end
     end
 end
 
-# %%
-const segment_map1 = Dict{Regex,String}(
-        r"constant ([-+]?\d+)" => "@([-+]?\d+)",
-        r"temp ([0-7])" => "@R5\n",
-
-
-        r"local ([-+]?\d+)" => "@LCL\n"
-
-        SA[r"local ([-+]?\d+)", r"argument ([-+]?\d+)", r"this ([-+]?\d+)", r"that ([-+]?\d+)"],
-        SA[             "@LCL\n",                 "@ARG",            "@THIS",            "@THAT"]
+function translate_vm(stream_in::IO, stream_out::IO, )
+    arithmetic_logical_map = Dict{Regex,Int}(
+        r"add" => 0, r"sub" => 0, r"neg" => 0,
+        r"eq"  => 0, r"gt"  => 0, r"lt"  => 0,
+        r"and" => 0, r"or"  => 0, r"not" => 0,
     )
-const segment_map2 = Dict{Regex,String}(
-        SA[r"pointer 0", r"pointer 1"],
-        SA[     "@THIS",      "@THAT"]
-    )
-const segment_map3 = StaticDictionary{5,Regex,String}(SA[r"temp ([0-7])"], SA["@R5"])
-const segment_map4 = StaticDictionary{5,Regex,String}(SA[r"constant ([-+]?\d+)"], SA["@([-+]?\d+)"])
 
-function translate_push(out_stream::IO, in_stream::IO)
-
-
-
-    if segment == constant::MemorySegment
-        write(stream, "D=$(index)\n")
-    elseif any(_x->segment ==_x,  SA[lcl::MemorySegment, argument::MemorySegment, this::MemorySegment])
-        write(stream, "D=$(Int16(segment)+index)\n")
+    for file_line in eachline(stream_in)
+        line = remove_comments(file_line)
+        translate_push_pop(line, stream_out)
+        translate_arithmetic_logic(line, stream_out, arithmetic_logical_map)
     end
-
-    match
-    if "constant"
-        "@$(7)"
-    end
-
-
-    wrtie(stream, "@SP\n")
-    wrtie(stream, "A=M\n")
-    wrtie(stream, "M=D\n")
-    wrtie(stream, "@SP\n")
-    wrtie(stream, "M=M+1\n")
 end
 
 # %%
-function translate_pop(stream::IO, segment::MemorySegment, index::Int16)
-    #
-    write(stream, "@SP\n")
-    write(stream, "D=M\n")
+vm_files = [joinpath(@__DIR__, "MemoryAccess", "DummyTest", "DummyTest.vm"),
+            joinpath(@__DIR__, "MemoryAccess", "BasicTest", "BasicTest.vm"),
+            joinpath(@__DIR__, "MemoryAccess", "PointerTest", "PointerTest.vm"),
+            joinpath(@__DIR__, "MemoryAccess", "StaticTest", "StaticTest.vm"),
+            joinpath(@__DIR__, "StackArithmetic", "SimpleAdd", "SimpleAdd.vm"),
+            joinpath(@__DIR__, "StackArithmetic", "StackTest", "StackTest.vm")]
+asm_files = [joinpath(@__DIR__, "MemoryAccess", "DummyTest", "DummyTest.asm"),
+             joinpath(@__DIR__, "MemoryAccess", "BasicTest", "BasicTest.asm"),
+             joinpath(@__DIR__, "MemoryAccess", "PointerTest", "PointerTest.asm"),
+             joinpath(@__DIR__, "MemoryAccess", "StaticTest", "StaticTest.asm"),
+             joinpath(@__DIR__, "StackArithmetic", "SimpleAdd", "SimpleAdd.asm"),
+             joinpath(@__DIR__, "StackArithmetic", "StackTest", "StackTest.asm")]
 
-    #
-    write(stream, "@$(Int16(segment)+index)\n")
-    write(stream, "@SP\n")
-    write(stream, "M=M-1\n")
-
-    wrtie(stream, "@SP\nA=M\nM=D\n@SP\nM=M+1\n")
+for (f_in, f_out) in zip(vm_files, asm_files)
+    f_in = open(f_in,"r")
+    f_out = open(f_out,"w")
+    translate_vm(f_in, f_out,)
+    close(f_in)
+    close(f_out)
 end
-
-
-# %%
-using StaticTools
-using StaticArrays
-using BenchmarkTools
-
-iobuf = IOBuffer()
-
-@btime begin
-    # a = 3
-    # b = 4
-    # a + b
-    # SA["Heello","Heello","Heello"]
-    # "what $(3)"
-    # String(3)
-    # Base.ImmutableDict("Hello" => Int16(1), "Goodbye" => Int16(2))
-    # write($iobuf, Int16(3))
-    # write($iobuf, "3")
-    occursin(r"Alex (.*)", "Hello this is Alex speaking")
-    # write($iobuf, " ", string(3,base=10,pad=1))
-end
-
